4d90dae2a34fa32c5f04eb935cded16b40dcb909
Madhavesh Gohel <madhaveshgohel21@gmail.com>
2020-09-14 10:07:34 +0530

    Initial commit

---
+++ app/src/main/java/com/madhavesh/gohel/gallary/MyAdapter.java
@@ -0,0 +1,12 @@
+package com.madhavesh.gohel.gallary;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public  class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>
+{
+    private ArrayList<File>
+
+}
---
+++ app/src/main/java/com/madhavesh/gohel/gallary/cell.java
@@ -0,0 +1,21 @@
+package com.madhavesh.gohel.gallary;
+
+public  class cell {
+    public String path,title;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
